2cbd99fff3ba7eb3d0a6659eda1b30c074a90b34
Steven B <genseb7@gmail.com>
2023-03-23 09:36:46 -0400

    Upgrade tf (#10)
    * upgrade tf
    
    * update ci

--- versions.tf
+++ versions.tf
@@ -1,14 +1,10 @@
 terraform {
   required_version = ">= 0.14.8"
 
-  experiments = [
-    module_variable_optional_attrs
-  ]
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.36"
+      version = ">= 3.36"
     }
   }
 }
